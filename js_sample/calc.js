53364a81d18c2558f7e11c121e9ce9d53f6c87f2
Ajay Manikandan <AjayManikandan360@gmail.com>
2023-09-13 17:11:56 +0530

    calculation function test 1 ok 1

--- js_sample/calc.js
+++ js_sample/calc.js
@@ -4,3 +4,46 @@ function buttonPres(data)
     document.getElementById("inputScreen1").value+=data
 
 }
+function clearData() 
+{
+ 
+    document.getElementById("inputScreen1").value=""
+
+}
+function equalButton() {
+    // var text=document.getElementById("inputScreen1").value
+    // var result =eval(text)
+    // document.getElementById("inputScreen1").value=result
+    
+    let result
+    const operator = prompt(' +, -, * , /  ');
+    const number1 = parseFloat(prompt('Enter first number: '));
+    const number2 = parseFloat(prompt('Enter second number: '));
+    switch(operator) {
+        case '+':
+             result = number1 + number2;
+            
+            break;
+    
+        case '-':
+             result = number1 - number2;
+            
+            break;
+    
+        case '*':
+             result = number1 * number2;
+            
+            break;
+    
+        case '/':
+            if(number2 != 0){
+                result = number1 / number2;
+             }else{
+                result = Double.NaN; 
+             }
+             break;
+        default:
+            console.log('Invalid operator');
+            break;
+    }
+}
