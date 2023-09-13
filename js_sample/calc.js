d99e8d6039730a6b797aa43189b989b0fdc38d48
Ajay Manikandan <AjayManikandan360@gmail.com>
2023-09-13 21:50:36 +0530

    commit error fix working good

--- js_sample/calc.js
+++ js_sample/calc.js
@@ -3,4 +3,20 @@ function buttonPres(data)
   
     document.getElementById("inputScreen1").value+=data
 
+ 
+
+}
+function clearData() 
+{
+ 
+    document.getElementById("inputScreen1").value=""
+
+}
+function equalButton() {
+    var text=document.getElementById("inputScreen1").value
+    var result =eval(text)
+    document.getElementById("inputScreen1").value=result  
+     
+    
+
 }
