4c9ca59bbc2ca171eec4841be47e42b928299087
Ajay Manikandan <AjayManikandan360@gmail.com>
2023-09-13 18:15:49 +0530

    test calc working 2 need more modification

--- js_sample/calc.js
+++ js_sample/calc.js
@@ -11,39 +11,9 @@ function clearData()
 
 }
 function equalButton() {
-    // var text=document.getElementById("inputScreen1").value
-    // var result =eval(text)
-    // document.getElementById("inputScreen1").value=result
+    var text=document.getElementById("inputScreen1").value
+    var result =eval(text)
+    document.getElementById("inputScreen1").value=result
     
-    let result
-    const operator = prompt(' +, -, * , /  ');
-    const number1 = parseFloat(prompt('Enter first number: '));
-    const number2 = parseFloat(prompt('Enter second number: '));
-    switch(operator) {
-        case '+':
-             result = number1 + number2;
-            
-            break;
     
-        case '-':
-             result = number1 - number2;
-            
-            break;
-    
-        case '*':
-             result = number1 * number2;
-            
-            break;
-    
-        case '/':
-            if(number2 != 0){
-                result = number1 / number2;
-             }else{
-                result = Double.NaN; 
-             }
-             break;
-        default:
-            console.log('Invalid operator');
-            break;
-    }
 }
